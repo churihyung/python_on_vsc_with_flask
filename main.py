4bbc91d295d73ac5db757d8177a6d93c13707ab1
Shin <dlwk0807@gmail.com>
2022-01-26 22:00:02 +0900

    flask_in_templates

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask("SuperScrapper")
 
 @app.route("/")
 def home():
-    return ""
+  return render_template("home.html")
 
-app.run(host="127.0.0.1")
+@app.route("/report")
+def report():
+  word = request.args.get('word')
+  return render_template("report.html", SearchBy=word)
+
+
+
+app.run(host="0.0.0.0")
